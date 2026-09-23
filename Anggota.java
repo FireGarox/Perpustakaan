0f1525fb48bbda97a522b4ee73530c3483fe02e7
FireGarox <105446564+FireGarox@users.noreply.github.com>
2026-09-23 14:56:33 +0700

    Anggota.java

--- Anggota.java
+++ Anggota.java
@@ -7,13 +7,11 @@ public class Anggota {
         this.nomorAnggota = nomorAnggota;
     }
 
-    // Method anggota untuk meminjam buku
     public void aksiPinjamBuku(buku b) {
         System.out.println(">> Anggota [" + nama + "] mencoba meminjam buku: " + b.judul);
         b.pinjambuku();
     }
 
-    // Method anggota untuk mengembalikan buku
     public void aksiKembalikanBuku(buku b) {
         System.out.println(">> Anggota [" + nama + "] mencoba mengembalikan buku: " + b.judul);
         b.kembalikanbuku();
